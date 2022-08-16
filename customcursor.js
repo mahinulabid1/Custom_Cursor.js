27d00f1148b422fa5ca30d3c6812940afab9be5d
MahinulA <himahinulabid@gmail.com>
2022-08-17 02:43:31 +0600

    smoothCursor function successful

--- customcursor.js
+++ customcursor.js
@@ -1,6 +1,7 @@
 const cursor = document.createElement("img");
 let cursor_X_direction = "";
 let cursor_Y_direction = "";
+let eventx = "";
 cursor.classList.add("cursor");
 cursor.setAttribute("src", "../Cursor/c-1.png");
 document.body.appendChild(cursor);
@@ -8,43 +9,21 @@ let select_cursor = document.querySelector(".cursor");
 // get initial mouse location
 
 document.body.addEventListener("mousemove", (event) => {
-    console.log("mouse entered");
     let howMuchScrolled = window.pageYOffset;
+    eventx = event;
     let mouseCoorDination = event.clientY;
     let both = howMuchScrolled + mouseCoorDination;
-    // select_cursor.style.top = `${both}px`;  
-    // select_cursor.style.top = `${event.clientY}px`;
-    // select_cursor.style.left = `${event.clientX}px`;
+    select_cursor.style.top = `${both}px`;  
+    select_cursor.style.left = `${event.clientX}px`;
     cursor_X_direction = event.clientX;
     cursor_Y_direction = event.clientY;
-
-    select_cursor.style.transform = `translate( ${event.clientX}px,${both}px)`;
-    // select_cursor.style.left = `${event.clientX}px`;
-
 });
 
-document.body.addEventListener("scroll", (event) => {
-    // console.log('scrolled');
-    // console.log(event.target);
-    // var event = new Event('mousemove',{clientX:150, clientY: 150});
-    // let x =document.body.dispatchEvent(event);
-    // console.log(x);
+window.addEventListener("scroll", () => {
     let howMuchScrolled = window.pageYOffset;
-    console.log(event.clientX);
-    // let position_cursor = howMuchScrolled + cursor_Y_direction;
-    // console.log(position_cursor);
-    // var x = new Event('mousemove');
-
-    // document.body.dispatchEvent(x);
+    let both = howMuchScrolled + eventx.clientY;
+    select_cursor.style.top = `${both}px`;  
 })
 
 
 
-// const cursorMovement=(theEvent)=>{
-//     let howMuchScrolled = window.pageYOffset;
-//     let mouseCoorDination = theEvent.clientY;//have to return event argument
-//     let both = howMuchScrolled + mouseCoorDination;
-//     // console.log(both);
-//     select_cursor.style.top = `${both}px`;
-//     select_cursor.style.left = `${theEvent.clientX}px`;
-// }
