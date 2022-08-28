cee712fffb4f76bd2064f9c849903511f90ac4ae
MahinulA <himahinulabid@gmail.com>
2022-08-28 13:53:04 +0600

    added selector pointer

--- animatto_cursor.js
+++ animatto_cursor.js
@@ -2,17 +2,18 @@
 (() => {
     const defaultCursorPath = "https://onavyint.sirv.com/customcursor%2Cjs/Cursordefault/";
     const pointerCursorPath= "https://onavyint.sirv.com/customcursor%2Cjs/CursorPointer/";
-    const anyPointerElement = document.querySelectorAll('.onHover'); //check any hover elements exist
+    const textPointerCursorPath = "https://onavyint.sirv.com/customcursor%2Cjs/TextPointer/";
+    const anyPointerElement = document.querySelectorAll('.onHoverPointer'); //check any hover elements exist
+    const anyTextElement = document.querySelectorAll('.onHoverSelect'); //check any hover elements exist
 
     const cursor = document.createElement("img");
     cursor.classList.add("cursor");
 
     const Cur_Defaut_Arr = ["cursor1", "cursor2", "cursor3", "cursor4", "cursor5", "cursor6", "cursor7", "cursor8", "cursor9", "cursor10", "cursor11"];
     const Cur_Pointer_arr = ['pointer1', 'pointer2', 'pointer3', 'pointer4'];
+    const Cur_text_Pointer_arr  = ['textpointer1', 'textpointer2', 'textpointer3', 'textpointer4', 'textpointer5'];
 
     let cmd_element = document.querySelector(".create_cursor");
-
-
 
     const CursorDecider=(ArElement)=>{
         for(let i = 0; i < ArElement.length; i++){
@@ -25,35 +26,39 @@
         return ArElement[0]; //if no class is available then return first element
     }
 
-
-
     const DefaultCursor = `${defaultCursorPath}${CursorDecider(Cur_Defaut_Arr)}.png`;
     const PointerCursor =  `${pointerCursorPath}${CursorDecider(Cur_Pointer_arr)}.png`;
+    const TextPoniter =  `${textPointerCursorPath}${CursorDecider(Cur_text_Pointer_arr)}.png`;
 
     // initial cursor loading
     cursor.setAttribute("src", `${DefaultCursor}`);
 
-    if(anyPointerElement != null || anyPointerElement != undefined){
-        for(let i  = 0; i < anyPointerElement.length; i++){
-            let pointerEl = anyPointerElement[i];
-            console.log(pointerEl);
-            pointerEl.addEventListener("mouseenter", ()=>{
-                console.log("mouse entered");
-                cursor.setAttribute("src", `${PointerCursor}`);
-            });
-            pointerEl.addEventListener("mouseleave", ()=>{
-                cursor.setAttribute("src", `${DefaultCursor}`);
-            });
+    // event based cursor loading
+    let EventCursorLoad = (selectorElement, changeCursor) =>{
+        if(selectorElement != null || selectorElement != undefined){
+            for(let i  = 0; i < selectorElement.length; i++){
+                let pointerEl = selectorElement[i];
+                console.log(pointerEl);
+                pointerEl.addEventListener("mouseenter", ()=>{
+                    console.log("mouse entered");
+                    cursor.setAttribute("src", `${changeCursor}`);
+                });
+                pointerEl.addEventListener("mouseleave", ()=>{
+                    cursor.setAttribute("src", `${DefaultCursor}`);
+                });
+            }
         }
     }
 
-
+    EventCursorLoad(anyPointerElement, PointerCursor);
+    EventCursorLoad(anyTextElement, TextPoniter);
     document.body.appendChild(cursor);
 })();
 const select_cursor = document.querySelector(".cursor");
 // ===========================================================================================================================
 
-// SOME CSS STUFF
+
+// STYLES
 (()=>{
     const Cursor = document.querySelector(".cursor");
     Cursor.style.height =  "30px";
@@ -69,7 +74,6 @@ const select_cursor = document.querySelector(".cursor");
         AllSelector.style.cursor = "none";
     }
 })();
-
 
 
 // cursor movement handler
@@ -94,9 +98,6 @@ const select_cursor = document.querySelector(".cursor");
     });
 })();
 // =============================================================================================================================
-
-
-
 
 
 // cursor display handler
