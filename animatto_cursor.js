6214dbc6fb05867715ef0b2beec62e0d09bb7735
MahinulA <himahinulabid@gmail.com>
2022-08-28 13:36:51 +0600

    added pointer events

--- animatto_cursor.js
+++ animatto_cursor.js
@@ -1,21 +1,53 @@
 //creating an element
 (() => {
     const defaultCursorPath = "https://onavyint.sirv.com/customcursor%2Cjs/Cursordefault/";
+    const pointerCursorPath= "https://onavyint.sirv.com/customcursor%2Cjs/CursorPointer/";
+    const anyPointerElement = document.querySelectorAll('.onHover'); //check any hover elements exist
+
     const cursor = document.createElement("img");
     cursor.classList.add("cursor");
 
-    const Cur_Arr = ["cursor1", "cursor2", "cursor3", "cursor4", "cursor5", "cursor6", "cursor7", "cursor8", "cursor9", "cursor10", "cursor11"];
+    const Cur_Defaut_Arr = ["cursor1", "cursor2", "cursor3", "cursor4", "cursor5", "cursor6", "cursor7", "cursor8", "cursor9", "cursor10", "cursor11"];
+    const Cur_Pointer_arr = ['pointer1', 'pointer2', 'pointer3', 'pointer4'];
+
     let cmd_element = document.querySelector(".create_cursor");
 
-    Cur_Arr.map((el)=>{
-        let checkCursorClass =cmd_element.classList.contains(el);
-        console.log(checkCursorClass);
-        if(checkCursorClass == true){
-            cursor.setAttribute("src", `${defaultCursorPath}${el}.png`);
-        }
-    });
 
-    
+
+    const CursorDecider=(ArElement)=>{
+        for(let i = 0; i < ArElement.length; i++){
+            let classString = ArElement[i];
+            let checkCursorClass =cmd_element.classList.contains(classString);
+            if(checkCursorClass == true){
+                return classString;
+            }
+        }
+        return ArElement[0]; //if no class is available then return first element
+    }
+
+
+
+    const DefaultCursor = `${defaultCursorPath}${CursorDecider(Cur_Defaut_Arr)}.png`;
+    const PointerCursor =  `${pointerCursorPath}${CursorDecider(Cur_Pointer_arr)}.png`;
+
+    // initial cursor loading
+    cursor.setAttribute("src", `${DefaultCursor}`);
+
+    if(anyPointerElement != null || anyPointerElement != undefined){
+        for(let i  = 0; i < anyPointerElement.length; i++){
+            let pointerEl = anyPointerElement[i];
+            console.log(pointerEl);
+            pointerEl.addEventListener("mouseenter", ()=>{
+                console.log("mouse entered");
+                cursor.setAttribute("src", `${PointerCursor}`);
+            });
+            pointerEl.addEventListener("mouseleave", ()=>{
+                cursor.setAttribute("src", `${DefaultCursor}`);
+            });
+        }
+    }
+
+
     document.body.appendChild(cursor);
 })();
 const select_cursor = document.querySelector(".cursor");
